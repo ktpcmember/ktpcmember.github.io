5aebc743e57fda0a15bf321084330b407cb3582f
vlrfsg <63190949+vlrfsg@users.noreply.github.com>
2020-05-26 00:42:09 +0900

    unstablity fixed

--- header.js
+++ header.js
@@ -4,8 +4,10 @@ function ck(){
  var a=document.getElementById("hnmu");
  if(a.style.display=="none"){
   a.style.display="flex";
+  a.style.pointerEvents="auto";
  }else{
   a.style.display="none";
+  a.style.pointerEvents="none";
  }
  return;
 }
