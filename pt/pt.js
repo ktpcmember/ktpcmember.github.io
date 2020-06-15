6714211e0ee43255a3a556861af4c36b2b45f652
vlrfsg <63190949+vlrfsg@users.noreply.github.com>
2020-06-16 00:16:22 +0900

    video

---
+++ pt/pt.js
@@ -0,0 +1,15 @@
+﻿
+function ck(){
+ var a=document.getElementById("hnmu");
+ var b=document.getElementById("ib3_vo");
+ if(a.style.display=="none"){
+  a.style.display="flex";
+  a.style.pointerEvents="auto";
+  b.controls=false;
+ }else{
+  a.style.display="none";
+  a.style.pointerEvents="none";
+  b.controls=true;
+ }
+ return;
+}
