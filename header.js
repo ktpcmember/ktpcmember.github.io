03392c6472c38ae6d3e10fc6f8c3eb91713ad9e9
vlrfsg <63190949+vlrfsg@users.noreply.github.com>
2020-06-21 00:51:19 +0900

    more static

--- header.js
+++ header.js
@@ -1,17 +1,4 @@
 ﻿
-function fsticky(){
- var a=document.getElementById("mubr");
- if(a.getBoundingClientRect().height>window.innerHeight){
-  a.style.position="static";
- }else{
-  a.style.position="sticky";
- }
- return;
-}
-
-window.onload=fsticky;
-window.onresize=fsticky;
-
 function ck(){
  var a=document.getElementById("hnmu");
  if(a.style.display=="none"){
@@ -21,6 +8,5 @@ function ck(){
   a.style.display="none";
   a.style.pointerEvents="none";
  }
- fsticky();
  return;
 }
