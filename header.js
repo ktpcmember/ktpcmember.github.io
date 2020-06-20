7f6610538b4f03e30c98a7bc98c8525f89756cc4
vlrfsg <63190949+vlrfsg@users.noreply.github.com>
2020-06-20 23:27:41 +0900

    static

--- header.js
+++ header.js
@@ -1,4 +1,17 @@
 ﻿
+function fsticky(){
+ var a=document.getElementById("mubr");
+ if(a.getBoundingClientRect().height>window.innerHeight){
+  a.style.position="static";
+ }else{
+  a.style.position="sticky";
+ }
+ return;
+}
+
+window.onload=fsticky;
+window.onresize=fsticky;
+
 function ck(){
  var a=document.getElementById("hnmu");
  if(a.style.display=="none"){
