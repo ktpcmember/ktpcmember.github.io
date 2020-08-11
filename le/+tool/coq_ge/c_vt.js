9c3fd4be519be0ac798770cba68184e64a543c50
vlrfsg <63190949+vlrfsg@users.noreply.github.com>
2020-08-12 03:46:58 +0900

    le +tool coq_ge

---
+++ le/+tool/coq_ge/c_vt.js
@@ -0,0 +1,12 @@
+﻿
+function push(){
+ var i_pt=document.getElementById("i_pt").textContent;;
+ var ottt="",da,r_qt;
+ r_qt=new XMLHttpRequest();
+ r_qt.open("GET","https://www.ktpc.tokyo/le/+tools/coq_ge/_cps.txt",false);
+ r_qt.send(null);
+ da=r_qt.responseText;
+ ottt=da;
+ navigator.clipboard.writeText(ottt);
+ return;
+}
