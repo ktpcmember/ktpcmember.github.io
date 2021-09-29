18ec8075a79ecc5a75b5ad85f99c2e59bb1d5f85
vlrfsg <63190949+vlrfsg@users.noreply.github.com>
2021-09-29 19:40:37 +0900

    create le vlrfsasm

---
+++ le/vlrfsasm/q.bat
@@ -0,0 +1,4 @@
+
+@pushd ..\..\..\vlrfsasm
+vlrfsasm ..\ktpcmember.github.io\le\vlrfsasm\test.html ..\ktpcmember.github.io\le\vlrfsasm\test.txt ..\ktpcmember.github.io\le\vlrfsasm\rule.txt def\common.txt
+@popd
