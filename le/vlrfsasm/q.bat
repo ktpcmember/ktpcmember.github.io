7f38cbc5b44a1a4d68610503692ab87ad4002cfc
vlrfsg <63190949+vlrfsg@users.noreply.github.com>
2021-10-05 07:39:13 +0900

    change le vlrfsasm q.bat workdir

--- le/vlrfsasm/q.bat
+++ le/vlrfsasm/q.bat
@@ -1,4 +1,8 @@
 
-@pushd ..\..\..\vlrfsasm
-vlrfsasm ..\ktpcmember.github.io\le\vlrfsasm\test.html ..\ktpcmember.github.io\le\vlrfsasm\test.txt ..\ktpcmember.github.io\le\vlrfsasm\rule.txt ..\ktpcmember.github.io\le\vlrfsasm\lectureList.txt def\common.txt
+@rem @pushd ..\..\..\vlrfsasm
+@rem vlrfsasm ..\ktpcmember.github.io\le\vlrfsasm\test.html ..\ktpcmember.github.io\le\vlrfsasm\test.txt ..\ktpcmember.github.io\le\vlrfsasm\rule.txt ..\ktpcmember.github.io\le\vlrfsasm\lectureList.txt def\common.txt
+@rem @popd
+
+@pushd tool\vlrfsasm
+vlrfsasm ..\le\vlrfsasm\test.html ..\le\vlrfsasm\test.txt ..\le\vlrfsasm\rule.txt ..\le\vlrfsasm\lectureList.txt def\common.txt
 @popd
