2de38cb3c15d8bba8453077343ba8577c8e8b493
vlrfsg <63190949+vlrfsg@users.noreply.github.com>
2021-10-07 13:37:06 +0900

    vlrfsasm v0.5

--- le/vlrfsasm/q.bat
+++ le/vlrfsasm/q.bat
@@ -1,8 +1,4 @@
 
-@rem @pushd ..\..\..\vlrfsasm
-@rem vlrfsasm ..\ktpcmember.github.io\le\vlrfsasm\test.html ..\ktpcmember.github.io\le\vlrfsasm\test.txt ..\ktpcmember.github.io\le\vlrfsasm\rule.txt ..\ktpcmember.github.io\le\vlrfsasm\lectureList.txt def\common.txt
-@rem @popd
-
 @pushd tool\vlrfsasm
-vlrfsasm ..\le\vlrfsasm\test.html ..\le\vlrfsasm\test.txt ..\le\vlrfsasm\rule.txt ..\le\vlrfsasm\lectureList.txt def\common.txt
+vlrfsasm ..\le\vlrfsasm\test.html ..\le\vlrfsasm\test.log ..\le\vlrfsasm\test.vlra ..\le\vlrfsasm\rule.vlra ..\le\vlrfsasm\lectureList.vlra def\common.vlra
 @popd
