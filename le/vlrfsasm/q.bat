68fc4de34130ebced671f040a95af481eb9377bc
vlrfsg <63190949+vlrfsg@users.noreply.github.com>
2021-10-07 18:55:44 +0900

    add le vlrfsasm q.bat exit /b

--- le/vlrfsasm/q.bat
+++ le/vlrfsasm/q.bat
@@ -2,3 +2,4 @@
 pushd ..\..\tool\vlrfsasm
 vlrfsasm ../../le/vlrfsasm/test.html ../../le/vlrfsasm/test.log ../../le/vlrfsasm/test.vlra ../../le/vlrfsasm/rule.vlra ../../le/vlrfsasm/lectureList.vlra def/common.vlra
 popd
+exit /b
