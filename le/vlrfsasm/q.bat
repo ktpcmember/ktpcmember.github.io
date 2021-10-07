8da23bcd27141158b95f23515bb801d7ecc63e0c
vlrfsg <63190949+vlrfsg@users.noreply.github.com>
2021-10-07 16:51:33 +0900

    fix le vlrfsasm q.bat root

--- le/vlrfsasm/q.bat
+++ le/vlrfsasm/q.bat
@@ -1,4 +1,4 @@
 
-pushd tool\vlrfsasm
+pushd ..\..\tool\vlrfsasm
 vlrfsasm ../../le/vlrfsasm/test.html ../../le/vlrfsasm/test.log ../../le/vlrfsasm/test.vlra ../../le/vlrfsasm/rule.vlra ../../le/vlrfsasm/lectureList.vlra def/common.vlra
 popd
