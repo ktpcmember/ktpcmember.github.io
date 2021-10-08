b919eaa5aebb58e50a13d5a3f897c1115b8c2621
vlrfsg <63190949+vlrfsg@users.noreply.github.com>
2021-10-08 20:26:33 +0900

    wip le vlrfsasm test.vlra

--- le/vlrfsasm/q.bat
+++ le/vlrfsasm/q.bat
@@ -1,4 +1,4 @@
 
-vlrfsasm %targetDir%/test.html %targetDir%/test.log %targetDir%/test.vlra %targetDir%/rule.vlra %targetDir%/lectureList.vlra def/common.vlra
+vlrfsasm %targetDir%\test.html %targetDir%\test.log %targetDir%\test.vlra %targetDir%\rule.vlra %targetDir%\lectureList.vlra def\common.vlra
 
 exit /b
