bf18d62da6a7980aacf43e76bfbcc560522ba8b7
vlrfsg <63190949+vlrfsg@users.noreply.github.com>
2021-10-30 10:44:03 +0900

    rename le vlrfsasm source.vlra to source.txt

--- le/vlrfsasm/q.bat
+++ le/vlrfsasm/q.bat
@@ -1,4 +1,4 @@
 
-vlrfsasm %targetDir%\test.html %targetDir%\test.log %targetDir%\test.vlra %targetDir%\rule.vlra %targetDir%\lectureList.vlra %targetDir%\commonRule.vlra %targetDir%\vlrfsasmRule.vlra %targetDir%\htmlEmbed.vlra def\common.vlra %targetDir%\source.vlra
+vlrfsasm %targetDir%\test.html %targetDir%\test.log %targetDir%\test.vlra %targetDir%\rule.vlra %targetDir%\lectureList.vlra %targetDir%\commonRule.vlra %targetDir%\vlrfsasmRule.vlra %targetDir%\htmlEmbed.vlra def\common.vlra %targetDir%\source.txt
 
 exit /b
