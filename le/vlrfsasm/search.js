2a1aa54e6edce7b0963e9b90a3b8c834bf0b6f16
vlrfsg <63190949+vlrfsg@users.noreply.github.com>
2021-11-11 19:46:30 +0900

    add pathRule.vlra

--- le/vlrfsasm/search.js
+++ le/vlrfsasm/search.js
@@ -1,17 +1,42 @@
 
 function menu(){
- var a=document.getElementById("menuLink");
- if(a.style.display=="none"){
-  a.style.display="flex";
-  a.style.pointerEvents="auto";
- }else{
-  a.style.display="none";
-  a.style.pointerEvents="none";
- }
- return;
+  var a = document.getElementById("menuLink");
+  if(a.style.display=="none"){
+    a.style.display="flex";
+    a.style.pointerEvents="auto";
+  }else{
+    a.style.display="none";
+    a.style.pointerEvents="none";
+  }
+  return;
 }
 
 function scrollToTop(){
- window.scroll(0,0);
- return;
+  window.scroll(0,0);
+  return;
+}
+
+function copyScript(id){
+  navigator.clipboard.writeText(excludeComment(document.getElementById(id).textContent));
+  return;
+}
+
+function excludeComment(node){
+  var s = "";
+  switch(node.nodeName){
+   case "#text":
+    s = node.data;
+    break;
+   default:
+    for(child of node.childNodes){
+      if(
+        (child.nodeName != "SPAN") ||
+        !(child.classList.contains("qComMetaComment"))
+      ){
+        s += excludeComment(child);
+      }
+    }
+    break;
+  }
+  return s;
 }
