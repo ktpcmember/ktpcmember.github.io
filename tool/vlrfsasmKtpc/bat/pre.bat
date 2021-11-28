533dc3bd34eca8327411f04ba2d439cd011a4f9e
vlrfsg <63190949+vlrfsg@users.noreply.github.com>
2021-11-28 16:52:43 +0900

    fix tempDir

--- tool/vlrfsasmKtpc/bat/pre.bat
+++ tool/vlrfsasmKtpc/bat/pre.bat
@@ -1,5 +1,5 @@
 
-cd %tempRoot%\vlrfsasm
+cd %tempDir%\vlrfsasm
 vlrfsasm %targetRoot%\temp\textContent.vlra %targetRoot%\log\pre.log def\common.vlra %toolDir%\preprocess.vlra %toolDir%\escape.vlra %1
 cd %targetDir%
 exit /b
