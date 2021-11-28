d16fa84af999caefa7f7be3c94d55f9fd53b7339
vlrfsg <63190949+vlrfsg@users.noreply.github.com>
2021-11-28 17:06:57 +0900

    fix targetDir

--- tool/vlrfsasmKtpc/bat/main.bat
+++ tool/vlrfsasmKtpc/bat/main.bat
@@ -1,5 +1,5 @@
 
 cd %tempDir%\vlrfsasm
-vlrfsasm %~dpn1.html %targetRoot%\log\main.log def\common.vlra %targetRoot%\temp\textContent.vlra %toolDir%\rule.vlra %toolDir%\escape.vlra %toolDir%\le\lectureList.vlra %toolDir%\code\htmlEmbed.vlra %toolDir%\code\commonRule.vlra %toolDir%\code\pathRule.vlra %toolDir%\code\vlrfsasmRule.vlra %*
+vlrfsasm %~dpn1.html %targetDir%\log\main.log def\common.vlra %targetDir%\temp\textContent.vlra %toolDir%\rule.vlra %toolDir%\escape.vlra %toolDir%\le\lectureList.vlra %toolDir%\code\htmlEmbed.vlra %toolDir%\code\commonRule.vlra %toolDir%\code\pathRule.vlra %toolDir%\code\vlrfsasmRule.vlra %*
 cd %targetDir%
 exit /b
