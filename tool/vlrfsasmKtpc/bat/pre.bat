f5ca6c151e26a1a22bb4266207b2be8004c2ecc3
vlrfsg <63190949+vlrfsg@users.noreply.github.com>
2021-11-28 17:44:34 +0900

    echo 5 >

--- tool/vlrfsasmKtpc/bat/pre.bat
+++ tool/vlrfsasmKtpc/bat/pre.bat
@@ -1,6 +1,6 @@
 
 echo %targetDir%\log\pre.log
 cd %tempDir%\vlrfsasm
-vlrfsasm %targetDir%\temp\textContent.vlra %targetDir%\log\pre.log def\common.vlra %toolDir%\preprocess.vlra %toolDir%\escape.vlra %1
+vlrfsasm %targetDir%\temp\textContent.vlra %targetDir%\log\pre.log def\common.vlra %toolDir%\preprocess.vlra %toolDir%\escape.vlra %1 > %targetDir%\log\pre.log
 cd %targetDir%
 exit /b
