a07998955cbd61d17eebdb23bf2a4f70055ae45b
vlrfsg <63190949+vlrfsg@users.noreply.github.com>
2021-11-28 18:11:09 +0900

    add bat setup.txt

--- le/vlrfsasm/setup/q.bat
+++ le/vlrfsasm/setup/q.bat
@@ -1,4 +1,4 @@
 
-call %toolDir%\bat\pre.bat %targetDir%\textContent.txt
+call %toolDir%\bat\pre.bat %targetDir%\textContent.txt %targetDir%\setup.vlra
 call %toolDir%\bat\main.bat %targetDir%\setup.vlra %targetDir%\source\source.vlra.txt %targetDir%\source\assemble.bat %targetDir%\source\message.log
 exit /b
--- tool/vlrfsasmKtpc/bat/pre.bat
+++ tool/vlrfsasmKtpc/bat/pre.bat
@@ -1,5 +1,5 @@
 
 cd %tempDir%\vlrfsasm
-vlrfsasm %targetDir%\temp\textContent.vlra %targetDir%\log\pre.log def\common.vlra %toolDir%\main\preprocess.vlra %toolDir%\main\escape.vlra %1
+vlrfsasm %targetDir%\temp\textContent.vlra %targetDir%\log\pre.log def\common.vlra %toolDir%\main\preprocess.vlra %toolDir%\main\escape.vlra %1 %2
 cd %targetDir%
 exit /b
