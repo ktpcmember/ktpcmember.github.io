c77f2a7920a923e663457bcda61109799ee36193
vlrfsg <63190949+vlrfsg@users.noreply.github.com>
2021-12-09 17:09:53 +0900

    add replaceList.bin

--- tool/vlrfsasmKtpc/bat/main.bat
+++ tool/vlrfsasmKtpc/bat/main.bat
@@ -1,5 +1,5 @@
 
 cd %tempDir%\vlrfsasm
-vlrfsasm %~dpn1.html %targetDir%\log\main.log def\common.vlra %targetDir%\temp\textContent.vlra %toolDir%\main\rule.vlra %toolDir%\main\escape.vlra %toolDir%\le\lectureList.vlra %toolDir%\code\htmlEmbed.vlra %toolDir%\code\commonRule.vlra %toolDir%\code\pathRule.vlra %toolDir%\code\vlrfsasmRule.vlra %toolDir%\math\math.vlra %toolDir%\math\sizeList.bin %*
+vlrfsasm %~dpn1.html %targetDir%\log\main.log def\common.vlra %targetDir%\temp\textContent.vlra %toolDir%\main\rule.vlra %toolDir%\main\escape.vlra %toolDir%\le\lectureList.vlra %toolDir%\code\htmlEmbed.vlra %toolDir%\code\commonRule.vlra %toolDir%\code\pathRule.vlra %toolDir%\code\vlrfsasmRule.vlra %toolDir%\math\math.vlra %toolDir%\math\sizeList.bin %toolDir%\math\replaceList.bin %*
 cd %targetDir%
 exit /b
