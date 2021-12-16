653e4c5fba679c2f43fa5517bf857069b083764d
vlrfsg <63190949+vlrfsg@users.noreply.github.com>
2021-12-16 12:03:00 +0900

    add vlrakhc.yml

--- tool/vlrfsasmKtpc/bat/pre.bat
+++ tool/vlrfsasmKtpc/bat/pre.bat
@@ -1,5 +1,5 @@
 
 cd %tempDir%\vlrfsasm
-vlrfsasm %targetDir%\temp\textContent.vlra %targetDir%\log\pre.log def\common.vlra %toolDir%\main\preprocess.vlra %toolDir%\main\escape.vlra %1 %2
+vlrfsasm %targetDir%\temp\textContent.vlra %targetDir%\log\pre.log def\common.vlra %toolDir%\main\preprocess.vlra %toolDir%\main\escape.vlra %targetDir%\vkhc.txt
 cd %targetDir%
 exit /b
