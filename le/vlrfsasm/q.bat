dd16b12879a4e637cdb9c9ce120b88c1351490db
vlrfsg <63190949+vlrfsg@users.noreply.github.com>
2021-12-28 21:32:31 +0900

    add le vlrfsasm

--- le/vlrfsasm/q.bat
+++
@@ -1,4 +0,0 @@
-
-vlrfsasm %targetDir%\test.html %targetDir%\test.log %targetDir%\test.vlra %targetDir%\rule.vlra %targetDir%\lectureList.vlra %targetDir%\commonRule.vlra %targetDir%\vlrfsasmRule.vlra %targetDir%\htmlEmbed.vlra def\common.vlra %targetDir%\source.txt %targetDir%\pathRule.vlra
-
-exit /b
